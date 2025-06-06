6de6a67dc9b01368d746ba685d25406f41c85153
Tomoki108 <shinonome81@gmail.com>
2025-06-06 19:01:20 +0900

    handle req

--- app/main.go
+++ app/main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
 )
 
+type Transaction struct {
+	UserID      int    `json:"user_id"`
+	Amount      int    `json:"amount"`
+	Description string `json:"description"`
+}
+
 func main() {
 	http.HandleFunc("/transactions", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var req Transaction
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "Bad request: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+
 		if _, err := io.Copy(w, r.Body); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
